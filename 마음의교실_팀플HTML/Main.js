3802edbce711f12404bc4115677cd964582ad5ba
SaeByeol <127357453+Kim-SaeByeol@users.noreply.github.com>
2023-10-26 19:48:12 +0900

    Add files via upload

---
+++ 마음의교실_팀플HTML/Main.js
@@ -0,0 +1,17 @@
+< script type = "text/javascript"
+src = "/js/jquery-3.6.0.min.js" > < /script> <
+    script type = "text/javascript" >
+        //Controller에서 받은 세션에 저장된 값
+    const session_user_id = "<%=CmmUtil.nvl((String)session.getAttribute("
+SESSION_USER_ID "))%>";
+
+
+$(document).ready(function () {
+
+    $("#Login-Check").on("click", function () {
+
+        if
+
+    });
+}); <
+/script>
